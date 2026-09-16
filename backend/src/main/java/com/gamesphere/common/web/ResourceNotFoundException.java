750f6ac743bc1b56df003fad69cbdb5874d37818
Utsav Prakash <126964544+Voidup2@users.noreply.github.com>
2026-09-16 17:17:39 +0530

    refactor(common): remove duplicate not-found exception

--- backend/src/main/java/com/gamesphere/common/web/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.gamesphere.common.web;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
