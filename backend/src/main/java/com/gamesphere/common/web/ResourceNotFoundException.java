c2170bce1b2fa04f6a091b1bb42de9f491e90017
Utsav Prakash <126964544+Voidup2@users.noreply.github.com>
2026-09-17 17:41:30 +0530

    refactor(common): consolidate not-found exception hierarchy

--- backend/src/main/java/com/gamesphere/common/web/ResourceNotFoundException.java
+++ backend/src/main/java/com/gamesphere/common/web/ResourceNotFoundException.java
@@ -1,6 +1,10 @@
 package com.gamesphere.common.web;
 
-public class ResourceNotFoundException extends RuntimeException {
+/**
+ * @deprecated Use {@link com.gamesphere.common.exception.ResourceNotFoundException}.
+ */
+@Deprecated(forRemoval = false)
+public class ResourceNotFoundException extends com.gamesphere.common.exception.ResourceNotFoundException {
     public ResourceNotFoundException(String message) {
         super(message);
     }
